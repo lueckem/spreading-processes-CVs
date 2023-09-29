a015418e7d2d273924eadf2cc821cfa500c273e3
Marvin <marvin-luecke@web.de>
2023-09-29 10:46:59 +0200

    plot fix

--- examples/voter_model/regular/main.py
+++ examples/voter_model/regular/main.py
@@ -5,9 +5,9 @@ import time
 
 
 def main():
-    run_method()  # run the method, takes ~15 minutes on 16-core CPU
+    # run_method()  # run the method, takes ~15 minutes on 16-core CPU
     plot()  # plot results
-    validate()  # run and plot validation
+    # validate()  # run and plot validation
 
 
 def run_method():
@@ -30,7 +30,7 @@ def run_method():
 
 
 def plot():
-    plot3d()  # plot of transition manifold, creates file plot_tm
+    plot_tm()  # plot of transition manifold, creates file plot_tm
 
 
 def validate():
--- examples/voter_model/regular/plot.py
+++ examples/voter_model/regular/plot.py
@@ -4,40 +4,43 @@ from mpl_toolkits.mplot3d.axes3d import Axes3D
 from matplotlib.layout_engine import TightLayoutEngine
 
 
-def plot3d():
+def plot_tm():
+    plt.rcParams["font.size"] = 13
+
     xi = np.load("data/xi.npy")
 
-    fig = plt.figure()
+    fig = plt.figure(figsize=(4, 2.5))
     ax = fig.add_subplot(projection="3d")
-
-    ax.scatter(xi[:, 0], xi[:, 2], xi[:, 1], c=xi[:, 0])
 
     scale_x = 1.5
     scale_y = 1.5
     scale_z = 1
 
+    ax.scatter(xi[:, 0], xi[:, 2], xi[:, 1], c=-xi[:, 0])
+
     ax.set_xlabel(r"$\varphi_1$", labelpad=-13)
     ax.set_ylabel(r"$\varphi_2$", labelpad=-13)
     ax.set_zlabel(r"$\varphi_3$", labelpad=-16)
+    # ax.set_aspect('equal')
 
     ax.set_yticklabels([])
     ax.set_xticklabels([])
     ax.set_zticklabels([])
 
     custom_lim = (np.min(xi[:, 0]), np.max(xi[:, 0]))
-    custom_lim2 = (0.4 * np.min(xi[:, 0]), 0.4 * np.max(xi[:, 0]))
+    custom_lim2 = (0.9 * np.min(xi[:, 0]), 0.9 * np.max(xi[:, 0]))
     plt.setp(ax, xlim=custom_lim, ylim=custom_lim2, zlim=custom_lim2)
 
     ax.get_proj = lambda: np.dot(
         Axes3D.get_proj(ax), np.diag([scale_x, scale_y, scale_z, 1])
     )
+    # ax.text2D(-0.15, 0.85, "(a)", transform=ax.transAxes, fontsize=15)
 
-    ax.view_init(18, -108, 0)
+    ax.view_init(15, -127, 0)
 
     layout = TightLayoutEngine(pad=-0.4)
     layout.execute(fig)
-    fig.subplots_adjust(right=0.95)
+    fig.subplots_adjust(right=1.05)
 
-    # plt.tight_layout()
     plt.savefig("plots/plot_tm.pdf")
     plt.show()
