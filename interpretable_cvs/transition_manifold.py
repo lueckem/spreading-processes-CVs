da6be7e94b68891c4f334f3ac093d42869e19854
Marvin <marvin-luecke@web.de>
2023-12-21 13:53:47 +0100

    dimension estimation in transition manifold

--- interpretable_cvs/transition_manifold.py
+++ interpretable_cvs/transition_manifold.py
@@ -7,39 +7,88 @@ class TransitionManifold:
     def __init__(
         self,
         bandwidth_transitions: float,
-        bandwidth_diffusion_map: float,
+        bandwidth_diffusion_map: float = 1,
         dimension: int = 10,
     ):
         self.bandwidth_transitions = bandwidth_transitions
         self.bandwidth_diffusion_map = bandwidth_diffusion_map
-        self.dimension = dimension
+        self.num_coordinates = dimension
         self.eigenvalues = None
+        self.eigenvectors = None
+        self.distance_matrix = None
+        self.dimension_estimate = None
 
-    def fit(self, x_samples: np.ndarray):
+    def fit(self, x_samples: np.ndarray, optimize_bandwidth: bool = False):
         """
         Parameters
         ----------
         x_samples : np.ndarray
             Array containing the endpoints of num_samples simulations for each anchor point.
             Shape = (num_anchor_points, num_samples, dimension).
-
+        optimize_bandwidth : bool, optional
+            If true, the diffusion_bandwidth is optimized.
+            This also yields an estimation of the transition manifold dimension, self.dimension_estimate.
         Returns
         -------
         np.ndarray
             Array containing the coordinates of each anchor point in diffusion space.
             Shape = (num_anchor_points, dimension).
         """
-        distance_matrix, _ = _numba_dist_matrix_gaussian_kernel(
+        self.set_distance_matrix(x_samples)
+        if optimize_bandwidth:
+            self.optimize_bandwidth_diffusion_maps()
+        return self.calc_diffusion_map()
+
+    def set_distance_matrix(self, x_samples: np.ndarray):
+        self.distance_matrix, _ = _numba_dist_matrix_gaussian_kernel(
             x_samples, self.bandwidth_transitions
         )
-        return self._calc_diffusion_map(distance_matrix)
 
-    def _calc_diffusion_map(self, distance_matrix: np.ndarray):
-        eigenvalues, eigenvectors = calc_diffusion_maps(
-            distance_matrix, self.dimension, self.bandwidth_diffusion_map
+    def optimize_bandwidth_diffusion_maps(self):
+        """
+        Optimize and set diffusion bandwidth by calculating
+        d log(S(e)) / d log(e)
+        where S(e) is the average of the kernel matrix K = exp(-D^2 / e).
+
+        Returns
+        -------
+        tuple(np.ndarray, np.ndarray, np.ndarray)
+            e, S(e), d log(S(e)) / d log(e)
+        """
+        if self.distance_matrix is None:
+            raise RuntimeError("No distance matrix available. Call the set_distance_matrix method first!")
+
+        epsilons = np.logspace(-6, 2, 101)
+        s = []
+        for epsilon in epsilons:
+            s.append(self.average_kernel_matrix(epsilon))
+        s = np.array(s)
+
+        derivative = _central_differences(np.log(epsilons), np.log(s))
+        optim_idx = np.argmax(derivative)
+        optim_epsilon = epsilons[optim_idx]
+        self.bandwidth_diffusion_map = optim_epsilon ** 0.5
+        self.dimension_estimate = derivative[optim_idx]
+
+        return epsilons, s, derivative
+
+    def calc_diffusion_map(self):
+        if self.distance_matrix is None:
+            raise RuntimeError("No distance matrix available. Call the set_distance_matrix method first!")
+
+        self.eigenvalues, self.eigenvectors = calc_diffusion_maps(
+            self.distance_matrix, self.num_coordinates, self.bandwidth_diffusion_map
         )
-        self.eigenvalues = eigenvalues
-        return eigenvectors.real[:, 1:] * eigenvalues.real[np.newaxis, 1:]
+        return self.eigenvectors.real[:, 1:] * self.eigenvalues.real[np.newaxis, 1:]
+
+    def average_kernel_matrix(self, epsilon) -> float:
+        """
+        epsilon = (bandwidth_diffusion_map)^2
+        """
+        if self.distance_matrix is None:
+            raise RuntimeError("No distance matrix available. Call the set_distance_matrix method first!")
+
+        return np.mean(np.exp(-(self.distance_matrix ** 2) / epsilon))
 
 
 @njit(parallel=True)
@@ -149,11 +198,13 @@ def calc_diffusion_maps(
     return sla.eigs(kernel_matrix, num_components + 1, weight_matrix)
 
 
-def main():
-    x_samples = np.random.random((100, 10, 2))
-    tm = TransitionManifold(1, 1, 3)
-    tm.fit(x_samples)
-
-
-if __name__ == "__main__":
-    main()
+def _central_differences(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    """
+    Compute dy/dx via central differences.
+    """
+    out = np.zeros(len(x))
+    for i in range(len(x)):
+        upper_idx = min(i + 1, len(x) - 1)
+        lower_idx = max(i - 1, 0)
+        out[i] = (y[upper_idx] - y[lower_idx]) / (x[upper_idx] - x[lower_idx])
+    return out
